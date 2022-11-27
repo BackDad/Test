83ef95fa0b7f5bb48885f168a034f734858b646a
ManukJr <>
2022-11-27 18:26:25 +0300

    добавлено решение

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿
+// метод заполнения массива числами c клавиатуры
+void FillArray(string [] collection)
+{
+    Console.WriteLine("Вы выбрали метод с клавиатуры");
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.WriteLine($"Введите {index}-й элемент массива");
+        collection[index] = Console.ReadLine();
+        index++;
+    }
+}
+// метод печати массива
+void PrintArray( string [] col)
+{
+    int index = 0;
+    Console.Write("[ ");
+    while (index < col.Length-1)
+    {
+            Console.Write(col[index]+" ,");
+            index++;
+    }
+    Console.Write(col[index]);
+    Console.Write("]");
+    //Console.WriteLine();
+}
+// метод поиска строки с заданной длиной
+string [] SelectOfString(string []array)
+{
+int index = 0;
+int k =0;
+string [] newArray = new string [array.Length];
+while( index < array.Length)
+{
+    if (array[index].Length <=3)
+    {
+        newArray[k]=array[index];
+        k++;
+        index++;
+    }
+    else index++;
+}
+return newArray;
+}
+
+while(true)
+{
+Console.Clear();
+int size = new Random().Next(3,5);
+ Console.WriteLine("Создан массив размером "+ size);
+ string [] ArrayOfRanum = new string [size];
+FillArray(ArrayOfRanum);
+PrintArray(ArrayOfRanum);
+Console.Write("->");
+PrintArray(SelectOfString(ArrayOfRanum));
+Console.WriteLine();
+Console.Write("<Enter  продолжение > <Пробел> для выхода ... ");
+        if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+            break;
+}
